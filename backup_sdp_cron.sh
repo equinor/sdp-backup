e36e565f7c46304f9f4d6bc4788abbbb1b1f8896
Stig Oskar Myhre Ofstad <stoo@statoil.com>
2018-01-19 12:43:43 +0000

    Updated name on shell script

--- backup_sdp_cron.sh
+++ backup_sdp_cron.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")/SDP-backup"
+cd ./SDP-backup
 git pull https://oauth2:####CHANGEME####@git.statoil.no/sdp/SDP-backup.git
-chmod u+x backup_sdp.bash
-./backup_sdp.bash
+chmod u+x backup_sdp.sh
+./backup_sdp.sh
