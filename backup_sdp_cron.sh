773fe71e4fa4cdb052ecb5d907788f68ae2ce4e1
Stig Oskar Myhre Ofstad <stoo@statoil.com>
2018-01-19 09:31:20 +0000

    removed old token

--- backup_sdp_cron.sh
+++ backup_sdp_cron.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")/SDP-backup"
-git pull https://oauth2:jnSNZvui5kGDEwSsZLyy@git.statoil.no/sdp/SDP-backup.git
+git pull https://oauth2:####CHANGEME####@git.statoil.no/sdp/SDP-backup.git
 chmod u+x backup_sdp.bash
 ./backup_sdp.bash
