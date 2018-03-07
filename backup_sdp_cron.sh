abb92965602b5beb1e6a843c6838ff3451157dc1
Stig Oskar Myhre Ofstad <stoo@statoil.com>
2018-03-07 12:00:44 +0000

    Added some comments.

--- backup_sdp_cron.sh
+++ backup_sdp_cron.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# This script makes sure the latest target.csv file is used in the backup job
+# It does this by pulling from git, so any changes that is pushed in 
+# the target.csv file will be reflected in the next backup job.
 cd $(dirname $0)/SDP-backup
 git pull https://oauth2:####CHANGEME####@git.statoil.no/sdp/SDP-backup.git
 chmod u+x backup_sdp.sh
