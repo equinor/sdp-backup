ca52a0ce64b5f70d928b041c9e6f771d8d18a16d
stoo <stoo@statoil.com>
2018-05-04 08:22:59 +0200

    fix: last commit. Option placement

--- backup_sdp.sh
+++ backup_sdp.sh
@@ -77,7 +77,7 @@ do
     done
 
     echo "Working on node: $ALT_HOSTNAME..."
-    rsync -ae "ssh --delete-during -o StrictHostKeyChecking=no" \
+    rsync -ae "ssh -o StrictHostKeyChecking=no" --delete-during \
     ${ROPTION[@]} root@$COMPUTERNAME:$DIR \
     $CNAME_PATH
 
