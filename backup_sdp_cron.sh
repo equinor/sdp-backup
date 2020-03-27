742909c1e2fb9e4326a83c3c41ddad98091d13df
solomson <auls@equinor.com>
2020-03-27 12:54:02 +0100

    git.statoil.no --> git.equinor.com

--- backup_sdp_cron.sh
+++ backup_sdp_cron.sh
@@ -3,6 +3,6 @@
 # It does this by pulling from git, so any changes that is pushed in 
 # the target.csv file will be reflected in the next backup job.
 cd $(dirname $0)/SDP-backup
-git pull https://oauth2:####CHANGEME####@git.statoil.no/sdp/SDP-backup.git
+git pull https://oauth2:####CHANGEME####@git.equinor.com/sdp/SDP-backup.git
 chmod u+x backup_sdp.sh
 ./backup_sdp.sh
